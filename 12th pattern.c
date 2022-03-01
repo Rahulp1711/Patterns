233f3bd3cdbc7af7063e1295097ef49a62ca5f9b
Rahulp1711 <99257880+Rahulp1711@users.noreply.github.com>
2022-03-01 15:28:07 +0530

    Todays Task Pattern program uploaded

---
+++ 12th pattern.c
@@ -0,0 +1,20 @@
+//  12  //
+
+#include <stdio.h>
+
+int main()
+{
+    int i,j,k;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<i;j++)
+        {
+            printf(" ");
+        }
+        for(k=1;k<=5;k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
---
+++ 7th pattern.c
@@ -0,0 +1,16 @@
+// 7 //
+
+#include <stdio.h>
+
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
